7f5a4bae54b8d0f652e3bb6c2ddd08b1b07e941b
Vanshika <118618760+Vanshikaduggal@users.noreply.github.com>
2025-02-05 14:57:29 +0530

    Update Task-05-02-2025.java

--- Task-05-02-2025.java
+++ Task-05-02-2025.java
@@ -13,3 +13,23 @@ class Main{
 		}
 	}
 }
+
+//COMMAND PROMPT :
+	C:\Users\vansh\OneDrive>cd Documents
+
+C:\Users\vansh\OneDrive\Documents>javac new.java
+new.java:1: error: class Main is public, should be declared in a file named Main.java
+public class Main{
+       ^
+new.java:5: error: incompatible types: int cannot be converted to String
+                        String[] arr={"Vanshika","Neeraj",11};
+                                                          ^
+2 errors
+
+C:\Users\vansh\OneDrive\Documents>javac new.java
+new.java:5: error: incompatible types: int cannot be converted to String
+                        String[] arr={"Vanshika","Neeraj",11};
+                                                          ^
+1 error
+
+C:\Users\vansh\OneDrive\Documents>
